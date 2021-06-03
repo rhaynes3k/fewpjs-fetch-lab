09a0ed0cfacea73c37440dccc3c660c9ae2385bd
rhaynes3k <rhaynes3k@gmail.com>
2021-06-02 19:53:05 -0500

    commit tree 8786a4041cfdb340ac505d34cfeaf694ee3f2f70

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books') .then(function(response) {
-    return fetch('https://anapioficeandfire.com/api/books')
-    .then(function(response){
-        return response.json()
-    }) .then(function(json){
-        renderBooks(json)
-    })
-  })
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 
 }
 
